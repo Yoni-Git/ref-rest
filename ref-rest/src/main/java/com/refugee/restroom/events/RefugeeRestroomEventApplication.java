6b75009cf913235fe39740464ae3fc87afd23d35
Yoni-Git <Yonatan.Goshime@Gmail.com>
2018-11-09 09:43:03 -0500

    Adding the inital project frame

---
+++ ref-rest/src/main/java/com/refugee/restroom/events/RefugeeRestroomEventApplication.java
@@ -0,0 +1,12 @@
+package com.refugee.restroom.events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefugeeRestroomEventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RefugeeRestroomEventApplication.class, args);
+    }
+}
